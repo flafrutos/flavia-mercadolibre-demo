4686bf31030c0e2767ec420e38cc8986c9010a63
flavia <flafrutos@gmail.com>
2022-04-02 18:17:37 -0300

    mi first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Servidor en puerto 3000")
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+//Le decis que es de recurso estatito
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get("");
