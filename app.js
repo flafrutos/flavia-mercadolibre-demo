f6cab37351c3541b2fe001259fe9a9ae6da4b55e
flavia <flafrutos@gmail.com>
2022-04-02 18:44:36 -0300

    3d commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.listen(process.env.PORT || 3000, () =>
   console.log("Servidor en puerto 3000")
 );
-
+app.listen(PORT);
 app.use(express.static(path.join(__dirname, "public")));
 //Le decis que es de recurso estatito
 
