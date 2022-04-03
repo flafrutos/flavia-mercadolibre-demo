28e02e6695836a7129a3685fda53c8a365ba22bb
flavia <flafrutos@gmail.com>
2022-04-02 21:41:12 -0300

    9 commit

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Servidor en puerto 3000")
-);
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT);
 
 app.use(express.static(path.join(__dirname, "public")));
